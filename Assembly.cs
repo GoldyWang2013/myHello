a00fecfe22505977e694025c728c946fbb72e5f6
Cody303 <470622166@qq.com>
2023-03-12 12:01:59 +0800

    Add files via upload

---
+++ Assembly.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+namespace Fx.Core
+{
+    static public class TAssembly
+    {
+        //public static void Init(TAppEnv appEnv)
+        //{
+        //    Assembly a = Assembly.GetExecutingAssembly();
+        //    appEnv.Cache.DictCache.Load(a);
+        //    TMetaRegistry.RegisterEntities(a);
+        //}
+        //
+        //
+        //
+
+
+    }
+}
